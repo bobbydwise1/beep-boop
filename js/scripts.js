e4f1133fa21cf4d75807f5decb14d2f1d7b33d4b
Robert Lee <bobbydwise1@hotmail.com>
2018-10-26 09:50:54 -0700

    Add simple input & ouput to index

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#inputForm").submit(function(event) {
+    event.preventDefault();
+    var userNumberInput = $("input#userInput").val();
+    console.log(userNumberInput);
+    debugger;
+    $("span.userOutput").text(userNumberInput);
+
+
+  }); /*This is the end brace for HTML button event monitoring*/
+}); /*This is the end brace for document ready*/
