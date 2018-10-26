5164f274bd00c021e63a99c4814d624f4ebaf6cc
Robert Lee <bobbydwise1@hotmail.com>
2018-10-26 11:39:45 -0700

    Add modulo 3 detection, and fixes to 0 and 1 detection

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,18 @@ function applyRulesToSerialNumberArray(arrayOfSerialIntegers) {
     var regex0 = RegExp("0");
     var regex1 = RegExp("1");
   arrayOfSerialIntegers.forEach(function(element) {
-      console.log(element);
-    if (regex0.test(element) === true) {
-      console.log(regex0.test(element) === true);
-      console.log("Element " + element + " contains char 0.");
-    } else {console.log("Element " + element + " DOESN'T contain char 0.");}
+    //  console.log(element);
+    if (element % 3 === 0 && element != 0) {
+      arrayOfSerialIntegers[element] = "I'm sorry Dave.  I'm afraid I can't do that.";
+    } else if (regex1.test(element) === true) {
+      arrayOfSerialIntegers[element] = "Boop!";
+    } else if (regex0.test(element) === true) {
+      arrayOfSerialIntegers[element] = "Beep!";
+    } else {
+      console.log("Number does not change");
+    }
   });
+  return arrayOfSerialIntegers;
 }
 
 $(document).ready(function() {
@@ -32,11 +38,10 @@ $(document).ready(function() {
     // console.log(userNumberInput);
     if (checkNumberIfPositiveInteger(userNumberInput) === 1) {
       var userArray = makeNewArrayZeroToUserInput(userNumberInput);
-      console.log(userArray);
-      applyRulesToSerialNumberArray(userArray);
-
-
-      $("span.userOutput").text(userArray);
+      console.log("This is the input: " + userArray);
+      var outputArray = applyRulesToSerialNumberArray(userArray);
+      console.log("This is the output: " + outputArray);
+      $("span.userOutput").text(outputArray);
     } else {
       $("span.userOutput").text("I'm sorry, your number is invalid.");
     }
